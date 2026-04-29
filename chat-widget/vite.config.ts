49772947cd71fdc4ceb519806d241219469ddb2d
alonso-be <alonso-be@gate.me>
2026-04-29 14:27:29 +0800

    feaet: tm-chat-widget

--- chat-widget/vite.config.ts
+++ chat-widget/vite.config.ts
@@ -3,6 +3,10 @@ import react from "@vitejs/plugin-react";
 
 export default defineConfig({
   plugins: [react()],
+  define: {
+    "process.env.NODE_ENV": JSON.stringify("production"),
+    "process.env": {},
+  },
   server: {
     port: 3001,
   },
